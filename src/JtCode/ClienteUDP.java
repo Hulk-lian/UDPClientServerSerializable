58c9cb3b0e44be963dade69862f9bd216e646734
Julián Troyano <juliantrodom@gmail.com>
2016-12-09 12:19:27 +0100

    .

--- src/JtCode/ClienteUDP.java
+++ src/JtCode/ClienteUDP.java
@@ -1,11 +1,11 @@
 package JtCode;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.SocketException;
-import java.net.UnknownHostException;
+import java.net.InetAddress;
 
 public class ClienteUDP {
 	private byte[] datosEnviados= new byte[255];
@@ -16,8 +16,29 @@ public class ClienteUDP {
 		BufferedReader lector=null;
 		try{
 			lector=new BufferedReader(new InputStreamReader(System.in));
-			
-		}catch(SocketException | UnknownHostException e){
+			while(true){
+				System.out.println("el mensaje a enviar");
+				String mensaje=lector.readLine();//leer la entrada por teclado
+				
+				if(mensaje.equals("Q")){
+					System.out.println("has decidido salir.");
+					break;//si metes solo una Q es que quieres salir
+				}
+				if(mensaje.trim().length()!=0){
+					String envio= "El cliente dice: "+mensaje;
+					datosEnviados=envio.getBytes();
+					
+					datosEnvi= new DatagramPacket(datosEnviados,datosEnviados.length,
+													InetAddress.getByName(Constantes.HOST_SERVER),
+													Constantes.PUERTO_SERVIDOR);					
+					socket.send(datosEnvi);
+					System.out.println("enviado el mensaje");
+				}
+				else{
+					System.out.println("no se envian mensajes vacios");
+				}
+			}
+		}catch(IOException e){
 			e.printStackTrace();
 		}
 		
